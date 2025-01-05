9a299d93d95dc28df750f9241eca8295f42ed1d5
Михаил Грасин <583michail@mail.ru>
2025-01-05 14:39:27 +0300

    Context (Interpretator) (WIP)

--- CPP-LTaTM/Precompiled.h
+++ CPP-LTaTM/Precompiled.h
@@ -3,11 +3,14 @@
 #include <string>
 #include <list>
 #include <vector>
+#include <unordered_map>
 #include <unordered_set>
+#include <stack>
 #include <iostream>
 #include <filesystem>
 #include <fstream>
 #include <sstream>
+#include <variant>
 
 using namespace std;
 
